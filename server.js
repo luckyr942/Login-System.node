d86b231741efacf1d2bd7151bce9af59927078e7
Lucky Raj Choudhary <112571987+luckyr942@users.noreply.github.com>
2024-02-08 22:52:47 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+const port = process.env.PORT || 4500;
+
+app.set("view engine", "ejs");
+
+//load static  assests
+app.use('/static', express.static(path.join(__dirname,'public')))
+
+
+//home route
+app.get('/', (req,res)=>{
+    res.render('base',{title:"Login System"});
+})
+
+app.listen(port,()=>{
+    console.log('Listening to the server on http://localhost:4500');
+});
